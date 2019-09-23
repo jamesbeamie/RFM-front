f048140eeec8a828f0c24f633fba55358f185e41
jamesbeamie <wafulajames9@gmail.com>
2019-09-23 18:54:07 +0300

    remove create react app code

--- src/components/containers/blogs/BlogItem.js
+++ src/components/containers/blogs/BlogItem.js
@@ -5,7 +5,7 @@ import '../../../assets/styles/blogItem.css';
 const BlogItem = (props) => (
 	<div key={props.blogId} className="col-md-4 home_list-item furem">
 		<button className="" onClick={props.specificBlog.bind(this, props.slug)}>
-			<img className="img-fluid" src={props.image_path} />
+			<img className="img-fluid" src={props.image_path} alt="blogImage"/>
 		</button>
 		<p className="tagi">tag:{props.tag}</p>
 	</div>
