35b9d6083f05a622805dd0357fca3c924141a952
jamesbeamie <wafulajames9@gmail.com>
2019-10-06 09:40:33 +0300

    white bottom

--- src/components/componentFiles/about.js
+++ src/components/componentFiles/about.js
@@ -19,17 +19,21 @@ import '../../assets/styles/about.css';
 const AboutPage = () => {
 	return (
 		<React.Fragment>
-
-
 			<section>
 				<div className="row container-fluid bg-light text-center">
-				
 					<div className="col-sm-6" id="wordding">
-						<img src={rose} className="img-fluid" alt="about"/>
+						<img src={rose} className="img-fluid" alt="about" />
 					</div>
 					<div className="col-sm-6 text-center py-5">
 						<h5>Meet Rose Brian</h5>
-						<p>She is your ordinary next door girl who loves God and uses her photography to showcase the beauty of God's creation. She is also a wife, mother and the lead photographer at Royal frames media. Her aim to capture your life's experiences in the most artistic and creative way possible. She seeks to journey with you as you sail through one milestone to another and ensure that a few years down the line you are able to relive those experiences through the photographs captured.</p>
+						<p>
+							She is your ordinary next door girl who loves God and uses her photography to showcase the
+							beauty of God's creation. She is also a wife, mother and the lead photographer at Royal
+							frames media. Her aim to capture your life's experiences in the most artistic and creative
+							way possible. She seeks to journey with you as you sail through one milestone to another and
+							ensure that a few years down the line you are able to relive those experiences through the
+							photographs captured.
+						</p>
 					</div>
 				</div>
 			</section>
@@ -38,23 +42,29 @@ const AboutPage = () => {
 				<div className="container-fluid bg-light">
 					<div className="text-center py-3">
 						<h5>About Royalframes media</h5>
-						<hr></hr>
+						<hr />
 					</div>
 				</div>
 			</section>
 
 			<section>
 				<div id="wording" className="row container-fluid bg-light text-center">
-				
 					<div className="col-sm-4">
-					<h5>Who we are</h5>
+						<h5>Who we are</h5>
 						<p className="fonti">We are RoyalFramesMedia, </p>
-						<p className="fonti">a company founded by Rose Brian, motivated by the need to have more creative content creators</p>
+						<p className="fonti">
+							a company founded by Rose Brian, motivated by the need to have more creative content
+							creators
+						</p>
 					</div>
 					<div className="col-sm-4 text-center">
 						<h5>What we do</h5>
-						<p className="fonti">We tell your story in an artistic way, we capture your life experiences as you go through one milestone to another creatively. We help you relive your most precious memories through photos.</p>
-						</div>
+						<p className="fonti">
+							We tell your story in an artistic way, we capture your life experiences as you go through
+							one milestone to another creatively. We help you relive your most precious memories through
+							photos.
+						</p>
+					</div>
 					<div className="col-sm-4 text-center">
 						<h5>Contact info</h5>
 						<p className="fonti">
@@ -63,37 +73,33 @@ const AboutPage = () => {
 						<p className="fonti">
 							<FontAwesomeIcon icon={faAddressBook} /> royalframesmedia@gmail.com
 						</p>
-
 					</div>
 				</div>
 			</section>
 
 			<section id="plx">
 				<div className="container-fluid">
-					<div className="wtitlebtm text-left">
-						<h5>We are here to deliver what you ask for,</h5>
-						<h5>In the right quality, quantity</h5>
-						<h5>and keep the smiles going</h5>
+					<div className="wtitlebtm text-center">
+						<p>
+							We are here to deliver what you ask for,In the right quality, quantityand keep the smiles
+							going.
+						</p>
 					</div>
 				</div>
 			</section>
 
-
 			<section>
 				<div className="row container-fluid bg-light text-center">
-					<div className="col-sm-4">
-					</div>
+					<div className="col-sm-4" />
 					<div className="col-sm-4 text-center">
-					<span className="soc-media">
+						<span className="soc-media">
 							<SocialIcon url="http://instagram.com/royalframesmedia" />
 						</span>
 						<span className="soc-media">
 							<SocialIcon url="http://facebook.com/royalframesmedia" />
 						</span>
 					</div>
-					<div className="col-sm-4">
-						
-					</div>
+					<div className="col-sm-4" />
 				</div>
 			</section>
 		</React.Fragment>
