bcf7b4ced4e8d3f2c74ea9626d8a7f404428bfce
James <admin@James.local>
2019-10-31 08:27:47 +0300

    Remived bbutton display

--- src/components/containers/blogs/BlogItem.js
+++ src/components/containers/blogs/BlogItem.js
@@ -1,17 +1,24 @@
 /* eslint-disable react/prop-types */
-import React from 'react';
-import '../../../assets/styles/blogItem.css';
+import React from "react";
+import "../../../assets/styles/blogItem.css";
 const styles = {
-	paddingLeft: '0',
-	paddingRight: '0'
+  paddingLeft: "0",
+  paddingRight: "0"
 };
 
-const BlogItem = (props) => (
-	<div key={props.blogId} className="col-md-4 col-lg-4 col-xl-3 home_list-item furem" style={styles}>
-		<button className="blog-button" onClick={props.specificBlog.bind(this, props.slug)}>
-			<img className="img-fluid" src={props.image_path} alt="blogImage" />
-		</button>
-	</div>
+const BlogItem = props => (
+  <div
+    key={props.blogId}
+    className='col-sm-4 col-md-4 col-lg-3 col-xl-3 home_list-item furem'
+    style={styles}
+  >
+    <button
+      className='blog-button'
+      onClick={props.specificBlog.bind(this, props.slug)}
+    >
+      <img className='img-fluid' src={props.image_path} alt='blogImage' />
+    </button>
+  </div>
 );
 
 export default BlogItem;
