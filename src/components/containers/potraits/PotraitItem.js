2872e7d0e65460bfad7f615a67d1e6afe874d905
James <admin@James.local>
2019-10-31 11:06:12 +0300

    Update img style on all components

--- src/components/containers/potraits/PotraitItem.js
+++ src/components/containers/potraits/PotraitItem.js
@@ -1,15 +1,23 @@
 /* eslint-disable react/prop-types */
-import React from 'react';
-import '../../../assets/styles/blogItem.css';
+import React from "react";
+import "../../../assets/styles/blogItem.css";
 const styles = {
-	paddingLeft: '0',
-	paddingRight: '0',
-  };
+  paddingLeft: "0",
+  paddingRight: "0"
+};
 
-const PotraitItem = (props) => (
-	<div key={props.blogId} className="col-md-4 col-xl-3 furem" style={styles}>
-		<img className="img-fluid picha" src={props.image_path} alt="potraitImage" />
-	</div>
+const PotraitItem = props => (
+  <div
+    key={props.blogId}
+    className='col-sm-4 col-md-4 col-lg-3 col-xl-3 furem'
+    style={styles}
+  >
+    <img
+      className='img-fluid picha'
+      src={props.image_path}
+      alt='potraitImage'
+    />
+  </div>
 );
 
 export default PotraitItem;
